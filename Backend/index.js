7e9277c8ae615f9f74b05675e8ddf0f1296cb7b7
Jeet <jeetbaldha12@gmail.com>
2024-05-09 22:23:01 +0530

    change cors

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173"
+        origin: "https://codelink.jeetbaldha.tech"
     }
 });
 
@@ -63,7 +63,7 @@ io.on('connection', (socket) => {
         }
 
     })
-    // console.log(socket);
+    // console.log(socket); 
     socket.on('disconnect', () => {
         
     });
